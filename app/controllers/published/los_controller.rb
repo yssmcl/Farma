34ed46a30389997e676b2e8c69bb5a12c5341702
Diego Marczal <dmarczal@gmail.com>
2014-04-07 23:04:28 -0300

    Fixed lo find on lo shared view

--- app/controllers/published/los_controller.rb
+++ app/controllers/published/los_controller.rb
@@ -1,19 +1,20 @@
 class Published::LosController < ApplicationController
 
-  def show
-    if current_user.admin?
-      @lo = Lo.includes(:introductions, :exercises).find(params[:id])
-    else
-      if params[:team_id]
-        team = current_user.teams.find(params[:team_id])
-        @lo = team.los.find(params[:id]) if team
-      else
-        @lo = current_user.los.find(params[:id])
-      end
-    end
-  end
-
   #def show
-  #  @lo = Lo.includes(:introductions, :exercises).find(params[:id])
+  #  if current_user.admin?
+  #    @lo = Lo.includes(:introductions, :exercises).find(params[:id])
+  #  else
+  #    if params[:team_id]
+  #      team = current_user.teams.find(params[:team_id])
+  #      @lo = team.los.find(params[:id]) if team
+  #    else
+  #      @lo = current_user.los.find(params[:id])
+  #    end
+  #  end
   #end
+
+  # Its use by, preview, publihesd and shared view
+  def show
+    @lo = Lo.includes(:introductions, :exercises).find(params[:id])
+  end
 end
